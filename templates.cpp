5c5f11ffb6ebd0bd2408db3de1288c1c63ea47e9
jubal-R <jubalrosenbarker@gmail.com>
2016-08-28 09:40:25 -0500

    Updated templates

--- templates.cpp
+++ templates.cpp
@@ -13,10 +13,10 @@ Templates::Templates()
 string asmb = "section .text\n\tglobal _start:\n_start:\n\nsection .data\n";
 string c = "#include <stdio.h>\n\nint main(int argc,char *argv[]){\n\t\n\n\treturn 0;\n}";
 string cplusplus = "#include <iostream>\n\nusing namespace std;\n\nint main(){\n\t\n\n\treturn 0;\n}";
-string html = "<!doctype html>\n<head>\n\t<script type=\"text/javascript\">\n\t\n\t</script>\n</head>\n<body>\n\t\n</body>\n</html>";
+string html = "<!DOCTYPE html>\n<head lang=\"en-US\">\n\t<meta charset=\"UTF-8\">\n\t<link rel=\"stylesheet\" href=\"\" />\n\t<title></title>\n\n\t<script>\n\t\t\n\t</script>\n</head>\n<body>\n\t<header>\n\t\t\n\t</header>\n\n</body>\n</html>";
 string java = "public class name {\n\n\tpublic static void main(String args[]){\n\t\t\n\t}\n\n}";
-string php = "<?php\n\t\n?>";
-string css = "@font-face{}\n\nhtml\n{\n\n}\n\nbody\n{\n\n}\n\nh1, h2, h3, h4, h5, h6\n{\n\n}\n\np\n{\n\n}\n\na\n{\n\n}\n\nimg\n{\n\n}";
+string php = "<?php\n\n\t\n\n?>";
+string css = "* {\n\t\n}\n\nhtml {\n\t\n}\n\nbody {\n\tdisplay: block;\n\t\n}\n\nh1 {\n\t\n}\n\np {\n\t\n}\n\na {\n\t\n}\n\nimg {\n\t\n}";
 
 string languages[7] = {asmb, c, cplusplus, html, java, php, css};
 
